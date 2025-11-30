2df858d8f0ea5b5e5dec7917f975d61a924bc708
fts18 <anandsagardubey71568@gmail.com>
2025-11-30 15:20:09 +0530

    Upgrade react-scripts and restore Supabase v2 for compatibility

--- src/components/Login.js
+++ src/components/Login.js
@@ -35,10 +35,11 @@ class Login extends React.Component {
 
     this.handleGoogleLogin = async () => {
       try {
-        const { error } = await supabase.auth.signIn({
-          provider: 'google'
-        }, {
-          redirectTo: `${window.location.origin}/`
+        const { data, error } = await supabase.auth.signInWithOAuth({
+          provider: 'google',
+          options: {
+            redirectTo: `${window.location.origin}/`
+          }
         });
         if (error) throw error;
       } catch (error) {
